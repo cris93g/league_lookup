e7b8e3cd89dc514fb66af0152678bbd8b82df384
cris93g <37388115+cris93g@users.noreply.github.com>
2019-06-20 16:05:45 -0500

    added to the specific profile page

--- src/components/SpecificGame/SpecificGame.js
+++ src/components/SpecificGame/SpecificGame.js
@@ -2,33 +2,45 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class SpecificGame extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			game: []
-		};
-	}
-	componentDidMount() {
-		axios
-			.post(`/api/match`, {
-				matchId: this.props.match.params.gameId
-			})
-			.then(response => {
-				this.setState({ game: response.data });
-			});
-	}
-	render() {
-		let { game } = this.state;
-		console.log(game);
-		return (
-			<div>
-				{/* {
-                game ?(
-                    <p>{game.gameMode}</p>
-                )
-            } */}
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .post(`/api/match`, {
+        matchId: this.props.match.params.gameId
+      })
+      .then(response => {
+        this.setState({ game: response.data });
+      });
+  }
+  render() {
+    let { game } = this.state;
+    console.log(game);
+    return (
+      <div>
+        <p>{game.gameMode}</p>
+        <p>{game.gameType}</p>
+        <div>
+          {game.participantIdentities ? (
+            game.participantIdentities.map(participant => (
+              <p>{participant.player.summonerName}</p>
+              //   <img
+              // 						className="iconpic"
+              // 						src={`http://ddragon.leagueoflegends.com/cdn/9.12.1/img/profileicon/${
+              // 							participant.player.profileIconId
+              // 						}.png`}
+              // 					/>
+            ))
+          ) : (
+            <h1>not any matches</h1>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 export default SpecificGame;
